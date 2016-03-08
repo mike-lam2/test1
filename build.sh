3fa54ece78835f9f3f5e6074b73698fe36f4fb72
mike-lam2 <author@example.com>
2016-03-07 22:20:00 -0500

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-cd /var/www/html/DevOpsApplication
+cd /var/www/html/test1
 rem git fetch --all
 rem git reset --hard
 git pull --no-commit
-chmod a+x ../DevOpsApplication/*.sh
+chmod a+x ../test1/*.sh
 git show --abbrev-commit --no-notes --format=full --no-patch 2>&1 >> WebContent/WEB-INF/version.txt
 
--- git_test.php
+++ git_test.php
@@ -1,5 +1,5 @@
 <?php 
-$sh='/var/www/html/DevOpsApplication/build.sh';
+$sh='/var/www/html/test1/build.sh';
 echo "executing ".$sh."\n";
 echo exec($sh." 2>&1");
  
